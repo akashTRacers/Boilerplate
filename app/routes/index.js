7f32b29767d30b903df9a20e6e8cb212c26ee1ee
akash jain <akjain@grepruby.com>
2018-06-19 12:45:32 +0530

    Boiler Plate1

---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+const initArticles = require("./articles");
+const openHome= require("./openHome.js");
+const initRoutes = (app) => {
+    console.log("inside index.js");
+    app.use('/', openHome());
+    app.use('/articles', initArticles()); 
+}
+module.exports = initRoutes;
+
