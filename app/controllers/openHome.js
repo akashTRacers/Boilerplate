8e8d6771676969b5c266a3e6257e37d1c9cc4d52
akash jain <akjain@grepruby.com>
2018-08-22 17:26:38 +0530

    new updates

---
+++ app/controllers/openHome.js
@@ -0,0 +1,23 @@
+const  path='../views/index.pug';
+const articles= require('../models/article.js');
+class openHomeController{
+  static getHome(req,res)
+  {
+    console.log("inside getHome.js controllers")
+    articles.find({}, function(err, results){
+      console.log("level1 pass")
+      if(err)
+      {
+          console.log(err);
+      }
+      else{
+          //console.log("results are",results);
+          res.render(path,{
+          title:'Articles',
+          articles1:results
+          });
+      } 
+    })
+  }
+}
+module.exports =openHomeController;
